4e9df2f37dd37c107300961657d847ec157ac2ab
Saurav-grg <menemy2323@gmail.com>
2024-02-11 08:02:44 +0545

    update btn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import "./index.css";
 import Employee from "./components/employee";
 import { useState } from "react";
-
+//import { v4 as uuidv4 } from "uuid";
 function App() {
-  const [role, setRole] = useState("dev");
+  // const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Alexa",
+      role: "Developer",
+      img: "https://rb.gy/qbg78z",
+    },
+    {
+      id: 2,
+      name: "Melnsa",
+      role: "Developer",
+      img: "https://rb.gy/wayn80",
+    },
+    {
+      id: 3,
+      name: "ChunLee",
+      role: "Developer",
+      img: "https://rb.gy/qbg78z",
+    },
+    {
+      id: 4,
+      name: "Breksha",
+      role: "Developer",
+      img: "https://rb.gy/wh5mlh",
+    },
+    {
+      id: 5,
+      name: "Komugi",
+      role: "Developer",
+      img: "https://rb.gy/2u7lx5",
+    },
+    { id: 6, name: "Chenny", role: "Developer", img: "https://rb.gy/qbg78z" },
+  ]);
+  function updateEmp(id, newName, newRole) {
+    const updatedEmployees = employees.map((emp) => {
+      if (emp.id == id) {
+        return { ...emp, name: newName, role: newRole };
+      }
+      return emp;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   let empAccess = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {empAccess ? (
         <>
-          <input
-            type="text"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-          />
-          <Employee name="Saurav" role="intern" />
-          <Employee name="Gaurav" />
-          <Employee name="Manish" role={role} />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((emp) => {
+              return (
+                <Employee
+                  id={emp.id}
+                  key={emp.id}
+                  name={emp.name}
+                  role={emp.role}
+                  img={emp.img}
+                  updateEmp={updateEmp}
+                />
+              );
+            })}
+          </div>
+          {/* <div className="flex justify-center">
+            <button className="my-[10px] bg-transparent hover:bg-blue-500 text-blue-700 font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              +New Employee
+            </button>
+          </div> */}
         </>
       ) : (
         <p>Access denied</p>
--- src/components/employee.js
+++ src/components/employee.js
@@ -1,9 +1,27 @@
+import EditEmp from "./EditEmp";
+
 function Employee(props) {
   return (
-    <>
-      <h3>Employee {props.name}</h3>
-      <p>{props.role ? props.role : "No role"} </p>
-    </>
+    <div className="min-w-[150px] max-w-[350px] py-8 px-8 max-w-sm m-1 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        //class="object-cover w-24 block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        className="object-cover rounded-full h-[90px] w-[90px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={props.img}
+        alt="Woman's Face"
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">{props.role}</p>
+        </div>
+        <EditEmp
+          id={props.id}
+          updateEmp={props.updateEmp}
+          name={props.name}
+          role={props.role}
+        />
+      </div>
+    </div>
   );
 }
 
